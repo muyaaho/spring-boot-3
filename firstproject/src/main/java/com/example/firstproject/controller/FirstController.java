b305a45b1dbbf5233ef33a0abe44b2f8a59322d3
muyaaho <skanthsu715@gmail.com>
2024-02-21 21:35:48 +0900

    Fix: @GetMapping() 추가해서 url 요청 접수

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,10 +1,12 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
+    @GetMapping("/hi")
     public String niceToMeetYou() {
         // templates 디렉토리에 작성한 mustache 페이지 파일 이름을 리턴하면 greetings.mustache 파일을 반환한다
         return "greetings";
