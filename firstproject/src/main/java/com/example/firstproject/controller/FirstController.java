f66f1bb6a495409908e182c313b58d835148ba80
muyaaho <skanthsu715@gmail.com>
2024-02-21 21:40:29 +0900

    Add: 모델
    username이라는 변수 추가
    Model 클래스의 addAttribute 메서드를 사용한다

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,13 +1,15 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "지희");
         // templates 디렉토리에 작성한 mustache 페이지 파일 이름을 리턴하면 greetings.mustache 파일을 반환한다
         return "greetings";
     }
