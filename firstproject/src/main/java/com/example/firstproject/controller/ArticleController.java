0770651f59afe1b999c49a0b8d6f5945b57298c7
muyaaho <skanthsu715@gmail.com>
2024-02-21 23:19:41 +0900

    Add: 의존성 주입
    new ArticleRepository();를 하지 않아도 @Autowired 어노테이션을 사용하면 스프링 부트가 미리 생성해 놓은 객체를 바로 사용할 수 있다
    @Autowired가 컨트롤러의 필드에 붙으면 스프링 부트가 만들어 놓은 객체를 가져와 주입해준다

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
