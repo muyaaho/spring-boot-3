0b4bc0d17562cb665e9afeaf6c8b4aa6586bd758
muyaaho <skanthsu715@gmail.com>
2024-02-22 22:26:55 +0900

    Add: chap5 셀프체크
    MemberController에 회원 조회 기능 추가

--- firstproject/src/main/java/com/example/firstproject/controller/MemeberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemeberController.java
@@ -6,15 +6,19 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 public class MemeberController {
 
     @Autowired
-    private MemberRepository memeberRepository;
+    private MemberRepository memberRepository;
 
     @GetMapping("/signup")
     public String newMemberForm() {
@@ -27,8 +31,31 @@ public class MemeberController {
 
         Member member = form.toEntity();
         log.info(member.toString());
-        Member saved = memeberRepository.save(member);
+        Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        log.info(memberEntity.toString());
+
+        model.addAttribute("member", memberEntity);
+        log.info(model.toString());
+
+        // TODO: members/show
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        List<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+
+        // TODO: members/index
+        return "members/index";
     }
 }
