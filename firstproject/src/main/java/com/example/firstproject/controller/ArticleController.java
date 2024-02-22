89b3d285f6063ad294d79e955dc1007191b49929
muyaaho <skanthsu715@gmail.com>
2024-02-22 12:30:55 +0900

    Add: 모델에 데이터 등록
    모델 객체를 받아와 addAttriute() 메서드를 통해 모델에 데이터를 등록한다

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,11 +33,13 @@ public class ArticleController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable Long id) {
+    public String show(@PathVariable Long id, Model model) {
         log.info("id = " + id);
         // 1. id를 조회해 데이터 가져오기
         Optional<Article> articleEntity = Optional.ofNullable(articleRepository.findById(id).orElse(null));
+
         // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "";
     }
