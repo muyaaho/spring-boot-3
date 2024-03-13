c40066b3345197dc0cccfec8b05caf28a12bfe8e
muyaaho <skanthsu715@gmail.com>
2024-03-13 17:41:18 +0900

    Fix: Failed to execute SQL script statement #1 of file
    1. application.properties 변경
    2. data.sql -> import.sql(https://jjunii486.tistory.com/332)

--- firstproject_2/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO article(id, title, content) VALUES(1, "가가가", '111');
-INSERT INTO article(id, title, content) VALUES(2, "나나나", '222');
-INSERT INTO article(id, title, content) VALUES(3, "다다다", '333');
