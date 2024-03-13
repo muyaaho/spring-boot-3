bedd1e9fbe39fc209d57fd0dbdcb23d4d9ff1626
muyaaho <skanthsu715@gmail.com>
2024-03-13 16:01:39 +0900

    Add: chap5 셀프체크, 회원조회 기능 추가

--- firstproject_2/src/main/java/com/example/firstproject_2/controller/MemberController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/controller/MemberController.java
@@ -3,10 +3,13 @@ package com.example.firstproject_2.controller;
 import com.example.firstproject_2.dto.MemberForm;
 import com.example.firstproject_2.entity.Member;
 import com.example.firstproject_2.repository.MemberRepository;
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -30,5 +33,19 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        ArrayList<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "members/index";
     }
 }
--- firstproject_2/src/main/java/com/example/firstproject_2/entity/Member.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/entity/Member.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class Member {
 
@@ -16,18 +22,4 @@ public class Member {
     @Column
     private String password;
 
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
