969791f584a3076f8bd2ffdd4af5ce0b0ba16102
muyaaho <skanthsu715@gmail.com>
2024-03-13 11:16:00 +0900

    Add: 모델 추가
    1. {{}}: mustache 문법, 변수명을 적는다
    2. 모델: MVC 패턴에서 데이터를 관리하는 역할
    3. model.addAttribute: 모델에 변수 등록

--- firstproject_2/src/main/java/com/example/firstproject_2/controller/FirstController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/controller/FirstController.java
@@ -1,13 +1,15 @@
 package com.example.firstproject_2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "지희");
         return "greetings";
     }
 }
