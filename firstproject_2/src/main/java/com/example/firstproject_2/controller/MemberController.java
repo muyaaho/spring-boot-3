4bc39a423855d7a9f9933e9eb58befa78612ed77
muyaaho <skanthsu715@gmail.com>
2024-03-13 14:46:46 +0900

    Refact: chap4 셀프체크, DTO, controller 리펙토링

--- firstproject_2/src/main/java/com/example/firstproject_2/controller/MemberController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject_2.controller;
 import com.example.firstproject_2.dto.MemberForm;
 import com.example.firstproject_2.entity.Member;
 import com.example.firstproject_2.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -21,12 +23,12 @@ public class MemberController {
 
     @PostMapping("/join")
     public String createMember(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
 
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
--- firstproject_2/src/main/java/com/example/firstproject_2/dto/MemberForm.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/dto/MemberForm.java
@@ -1,24 +1,15 @@
 package com.example.firstproject_2.dto;
 
 import com.example.firstproject_2.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class MemberForm {
 
     private String email;
     private String password;
-
-    public MemberForm(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberForm{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 
     public Member toEntity() {
         return new Member(null, email, password);
