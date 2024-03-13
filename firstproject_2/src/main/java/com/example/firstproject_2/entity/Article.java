605c0ad11bf43d4d1d0f09c6e8fe7790aeaf5eb9
muyaaho <skanthsu715@gmail.com>
2024-03-13 21:02:22 +0900

    Add: 기본키 자동으로 관리하는 전략 추가

--- firstproject_2/src/main/java/com/example/firstproject_2/entity/Article.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/entity/Article.java
@@ -3,6 +3,7 @@ package com.example.firstproject_2.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +18,7 @@ import lombok.ToString;
 public class Article {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
--- firstproject_2/src/main/resources/data.sql
+++ firstproject_2/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(id, title, content) VALUES(1, 'aaa', '111');
-INSERT INTO article(id, title, content) VALUES(2, 'bbb', '222');
-INSERT INTO article(id, title, content) VALUES(3, 'ccc', '333');
+INSERT INTO article(title, content) VALUES('aaa', '111');
+INSERT INTO article(title, content) VALUES('bbb', '222');
+INSERT INTO article(title, content) VALUES('ccc', '333');
 
 INSERT INTO member(id, email, password) VALUES(1, 'aa@naver.com', 'aaa11');
 INSERT INTO member(id, email, password) VALUES(2, 'bb@naver.com', 'bbb22');
