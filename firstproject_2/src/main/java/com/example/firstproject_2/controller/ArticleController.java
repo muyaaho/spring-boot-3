d27f394176afb11f56f2e4509ee3ac483ada977a
muyaaho <skanthsu715@gmail.com>
2024-03-13 20:33:26 +0900

    Add: 삭제완료 메시지 남기기
    1. RedirectAttributes: 리다이렉트 페이지에서 사용할 데이터를 남길 수 있다
    2. layouts/header에 내용 추가

--- firstproject_2/src/main/java/com/example/firstproject_2/controller/ArticleController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -87,7 +88,7 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}/delete")
-    public String delete(@PathVariable Long id) {
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
         log.info("삭제 요청이 들어왔습니다.");
         // 1. 삭제할 대상 가져오기
         Article target = articleRepository.findById(id).orElse(null);
@@ -96,6 +97,7 @@ public class ArticleController {
         // 2. 대상 엔티티 삭제하기
         if (target != null) {
             articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
         }
         // 3. 결과 페이지로 리다이렉트하기
         return "redirect:/articles";
