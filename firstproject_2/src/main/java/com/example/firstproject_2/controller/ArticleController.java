7de5c10bbb8491141c195d06884619a73b219485
muyaaho <skanthsu715@gmail.com>
2024-03-13 13:27:46 +0900

    Add: DTO를 엔티티로 변환하기
    1. Article 클래스 생성(엔티티 클래스)
    2. toEntity(): 폼 데이터를 담은 DTO 객체를 엔티티로 반환

--- firstproject_2/src/main/java/com/example/firstproject_2/controller/ArticleController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject_2.controller;
 
 import com.example.firstproject_2.dto.ArticleForm;
+import com.example.firstproject_2.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,9 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        // 2. 리파지토리로 엔티티를 DB에 저장
         return "";
     }
 }
--- firstproject_2/src/main/java/com/example/firstproject_2/dto/ArticleForm.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.firstproject_2.dto;
 
+import com.example.firstproject_2.entity.Article;
+
 public class ArticleForm {
     private String title;
     private String content;
@@ -15,5 +17,9 @@ public class ArticleForm {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
     }
 }
