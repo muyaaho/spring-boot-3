6a6ac02562b1aabedda7ca10cb65287301fc0a52
muyaaho <skanthsu715@gmail.com>
2024-03-13 20:47:55 +0900

    Add: chap8 셀프체크) 삭제하기 버튼 누르고 삭제 메시지 출력

--- firstproject_2/src/main/java/com/example/firstproject_2/controller/MemberController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -69,5 +70,20 @@ public class MemberController {
         }
 
         return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다.");
+
+        Member target = memberRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+
+        return "redirect:/members";
     }
 }
--- firstproject_2/src/main/resources/data.sql
+++ firstproject_2/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO article(id, title, content) VALUES(1, 'aaa', '111');
 INSERT INTO article(id, title, content) VALUES(2, 'bbb', '222');
 INSERT INTO article(id, title, content) VALUES(3, 'ccc', '333');
+
+INSERT INTO member(id, email, password) VALUES(1, 'aa@naver.com', 'aaa11');
+INSERT INTO member(id, email, password) VALUES(2, 'bb@naver.com', 'bbb22');
+INSERT INTO member(id, email, password) VALUES(3, 'cc@naver.com', 'ccc33');
+
