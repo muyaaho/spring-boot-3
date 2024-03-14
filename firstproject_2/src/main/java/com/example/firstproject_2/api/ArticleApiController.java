9e4c0e6b146d1f76475954cf781e5b160323e464
muyaaho <skanthsu715@gmail.com>
2024-03-14 09:57:16 +0900

    Add: REST API, post 구현

--- firstproject_2/src/main/java/com/example/firstproject_2/api/ArticleApiController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/api/ArticleApiController.java
@@ -1,11 +1,14 @@
 package com.example.firstproject_2.api;
 
+import com.example.firstproject_2.dto.ArticleForm;
 import com.example.firstproject_2.entity.Article;
 import com.example.firstproject_2.repository.ArticleRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +26,11 @@ public class ArticleApiController {
     }
 
     // POST
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
     // PATCH
     // DELETE
 }
