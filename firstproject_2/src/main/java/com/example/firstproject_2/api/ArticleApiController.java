f759001bf32a49f286a78372e4a62f528a7813c5
muyaaho <skanthsu715@gmail.com>
2024-03-14 10:26:28 +0900

    Add: REST API, patch 구현
    1. @Slf4j는 클래스 위에 선언한다
    2. article: 입력받은 정보, target: 원래 정보
    3. patch(): 일부만 수정할 경우 다른 데이터는 null이 되는 경우를 방지하기 위해 만든 메소드, 기존 정보가 있다면 일부분을 덮어쓰기 한다

--- firstproject_2/src/main/java/com/example/firstproject_2/api/ArticleApiController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/api/ArticleApiController.java
@@ -4,13 +4,18 @@ import com.example.firstproject_2.dto.ArticleForm;
 import com.example.firstproject_2.entity.Article;
 import com.example.firstproject_2.repository.ArticleRepository;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ArticleApiController {
     @Autowired
@@ -31,6 +36,26 @@ public class ArticleApiController {
         Article article = dto.toEntity();
         return articleRepository.save(article);
     }
+
     // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        // 1. DTO -> 엔티티 변환하기
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2. 타깃 조회하기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3. 잘못된 요청 처리하기
+        if (target == null || !id.equals(article.getId())) {
+            log.info("잘못된 요청, id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        // 4. 업데이트 및 정상 응답(200)하기
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+
+    }
+
     // DELETE
 }
