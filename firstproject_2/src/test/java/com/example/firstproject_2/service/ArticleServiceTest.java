e174cc0375f66cce85ed2d91a7bc382f111a0bc4
muyaaho <skanthsu715@gmail.com>
2024-03-15 16:50:01 +0900

    Test: create()
    1. 실패 test: null인지 비교 검증한다
    2. toString(): null이라서 tostring을 붙이지 않는다

--- firstproject_2/src/test/java/com/example/firstproject_2/service/ArticleServiceTest.java
+++ firstproject_2/src/test/java/com/example/firstproject_2/service/ArticleServiceTest.java
@@ -2,6 +2,7 @@ package com.example.firstproject_2.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.firstproject_2.dto.ArticleForm;
 import com.example.firstproject_2.entity.Article;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,6 +46,33 @@ class ArticleServiceTest {
         Article expected = null;
         // 2. 실제 데이터
         Article article = articleService.show(id);
+        // 3. 비교 및 검증
+        assertEquals(expected, article);
+    }
+
+    @Test
+    void create_성공_title과_content만_있는_dto_입력() {
+        // 1. 예상 데이터
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+        // 2. 실제 데이터
+        Article article = articleService.create(dto);
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    void create_실패_id가_포함된_dto_입력() {
+        // 1. 예상 데이터
+        Long id = 4L;
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(id, title, content);
+        Article expected = null;
+        // 2. 실제 데이터
+        Article article = articleService.create(dto);
         // 3. 비교 및 검증
         assertEquals(expected, article);
     }
