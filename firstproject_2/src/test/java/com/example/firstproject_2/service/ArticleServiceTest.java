2096ce9c1fa72075fd6d5ce4b82fef3d1339bf28
muyaaho <skanthsu715@gmail.com>
2024-03-15 16:32:35 +0900

    Test: index()
    1. 예상 데이터와 실제 데이터가 같은지 비교한다

--- firstproject_2/src/test/java/com/example/firstproject_2/service/ArticleServiceTest.java
+++ firstproject_2/src/test/java/com/example/firstproject_2/service/ArticleServiceTest.java
@@ -2,6 +2,10 @@ package com.example.firstproject_2.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.firstproject_2.entity.Article;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +16,14 @@ class ArticleServiceTest {
     ArticleService articleService;
     @Test
     void index() {
+        // 1. 예상 데이터
+        Article a = new Article(1L, "aaa", "111");
+        Article b = new Article(2L, "bbb", "222");
+        Article c = new Article(3L, "ccc", "333");
+        List<Article> expected = new ArrayList<>(Arrays.asList(a, b, c));
+        // 2. 실제 데이터
+        List<Article> articles = articleService.index();
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), articles.toString());
     }
 }
