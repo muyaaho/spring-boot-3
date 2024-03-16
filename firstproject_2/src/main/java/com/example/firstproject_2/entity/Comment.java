da2c98419c9df5ee28d2b4c0c09047e3ebf212c1
muyaaho <skanthsu715@gmail.com>
2024-03-16 13:24:39 +0900

    Feat: 댓글 엔티티
    1. @ManyToOne: Comment 엔티티와 Article 엔티티를 다대일 관계로 설정한다
    2. @JoinColumn: 외래키 매핑, name 속성 값이 column명이다

---
+++ firstproject_2/src/main/java/com/example/firstproject_2/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.example.firstproject_2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="article")
+    private Article article;
+    @Column
+    private String nickname;
+    @Column
+    private String body;
+
+
+}
