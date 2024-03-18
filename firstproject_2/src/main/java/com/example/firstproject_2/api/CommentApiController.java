1c93c75f5f1d62ada801f4e29150a9507fe733a5
muyaaho <skanthsu715@gmail.com>
2024-03-18 10:03:35 +0900

    Feat: 요청을 받아 응답할 컨트롤러 만들기(post)

--- firstproject_2/src/main/java/com/example/firstproject_2/api/CommentApiController.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/api/CommentApiController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,7 +25,15 @@ public class CommentApiController {
         // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
+
     // 2. 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDTO> create(@PathVariable Long articleId, @RequestBody CommentDTO dto) {
+        // 서비스에 위임
+        CommentDTO createdDto = commentService.create(articleId, dto);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
     // 3. 댓글 수정
     // 4. 댓글 삭제
 }
