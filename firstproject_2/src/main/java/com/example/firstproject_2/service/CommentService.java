026e25e2234a797367eb7dccc6bc058e40234375
muyaaho <skanthsu715@gmail.com>
2024-03-18 09:58:06 +0900

    Refactor: comments 메서드의 반복문을 stream으로 개선

--- firstproject_2/src/main/java/com/example/firstproject_2/service/CommentService.java
+++ firstproject_2/src/main/java/com/example/firstproject_2/service/CommentService.java
@@ -6,6 +6,7 @@ import com.example.firstproject_2.repository.ArticleRepository;
 import com.example.firstproject_2.repository.CommentRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +18,9 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDTO> comments(Long articleId) {
-        // 1. 댓글 조회
-        List<Comment> comments = commentRepository.findByArticleId(articleId);
-        // 2. 엔티티 -> DTO 변환
-        List<CommentDTO> dtos = new ArrayList<>();
-        for (int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDTO dto = CommentDTO.createCommentDto(c);
-            dtos.add(dto);
-        }
-        // 3. 결과 반환
-        return dtos;
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(CommentDTO::createCommentDto)
+                .collect(Collectors.toList());
     }
 }
